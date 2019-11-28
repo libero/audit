3a673162db83889614c83c6657d9531bb3d7695e
Cory <c.donkin@elifesciences.org>
2019-11-28 11:43:08 +0000

    prettier and linting fixes

--- src/repo/audit.test.ts
+++ src/repo/audit.test.ts
@@ -3,35 +3,35 @@ import * as Knex from 'knex';
 import { AuditLogItem } from 'domain/types';
 
 describe('KnexAuditRepository', (): void => {
-  describe('putLog', ():void => {
-    it('inserts to audit table using knex', async ():Promise<void> => {
-      const mockKnex = (jest.fn(() => ({
-        insert: jest.fn(),
-      }) )as unknown) as Knex;
-      const mockAuditItem = {} as unknown as AuditLogItem;
-      const Audit = new KnexAuditRepository(mockKnex);
-      await Audit.putLog(mockAuditItem);
-      expect(mockKnex).toBeCalledWith('audit');
-      });
-      it('inserts audit item passed using knex', async (): Promise<void> => {
-        const mockInsert = jest.fn();
-        const mockKnex = (jest.fn(() => ({
-          insert: mockInsert,
-        }) )as unknown) as Knex;
-        const mockAuditItem = {} as unknown as AuditLogItem;
-        const Audit = new KnexAuditRepository(mockKnex);
-        await Audit.putLog(mockAuditItem);
-        expect(mockInsert).toBeCalledWith(mockAuditItem);
-      });
+    describe('putLog', (): void => {
+        it('inserts to audit table using knex', async (): Promise<void> => {
+            const mockKnex = (jest.fn(() => ({
+                insert: jest.fn(),
+            })) as unknown) as Knex;
+            const mockAuditItem = ({} as unknown) as AuditLogItem;
+            const Audit = new KnexAuditRepository(mockKnex);
+            await Audit.putLog(mockAuditItem);
+            expect(mockKnex).toBeCalledWith('audit');
+        });
+        it('inserts audit item passed using knex', async (): Promise<void> => {
+            const mockInsert = jest.fn();
+            const mockKnex = (jest.fn(() => ({
+                insert: mockInsert,
+            })) as unknown) as Knex;
+            const mockAuditItem = ({} as unknown) as AuditLogItem;
+            const Audit = new KnexAuditRepository(mockKnex);
+            await Audit.putLog(mockAuditItem);
+            expect(mockInsert).toBeCalledWith(mockAuditItem);
+        });
 
-      it('returns true on completion',  async ():Promise<void> => {
-        const mockKnex = (jest.fn(() => ({
-          insert: jest.fn(),
-        }) )as unknown) as Knex;
-        const mockAuditItem = {} as unknown as AuditLogItem;
-        const Audit = new KnexAuditRepository(mockKnex);
-        const returnValue = await Audit.putLog(mockAuditItem);
-        expect(returnValue).toBe(true);
+        it('returns true on completion', async (): Promise<void> => {
+            const mockKnex = (jest.fn(() => ({
+                insert: jest.fn(),
+            })) as unknown) as Knex;
+            const mockAuditItem = ({} as unknown) as AuditLogItem;
+            const Audit = new KnexAuditRepository(mockKnex);
+            const returnValue = await Audit.putLog(mockAuditItem);
+            expect(returnValue).toBe(true);
         });
     });
-  });
+});
