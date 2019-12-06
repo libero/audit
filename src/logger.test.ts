7bbc60f103fe317afc387b57a0a7343598ba072a
Cory <c.donkin@elifesciences.org>
2019-12-06 14:07:37 +0000

    upgrade pino types dep and uncomment logger tests

--- src/logger.test.ts
+++ src/logger.test.ts
@@ -1,18 +1,16 @@
-// import Logger, { InfraLogger, DomainLogger } from './logger';
-/**
- * TODO: uncomment this test once @types/pino has type def for logger.bindings() see: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/40826
- */
+import Logger, { InfraLogger, DomainLogger } from './logger';
+
 describe('logger', () => {
     it('Logger has no bindings', () => {
-        // const loggerBindings = Logger.bindings();
-        // expect(loggerBindings).toEqual({})
+        const loggerBindings = Logger.bindings();
+        expect(loggerBindings).toEqual({});
     });
     it('InfraLogger has service set to audit and realm set to infra on bindings', () => {
-        // const loggerBindings = InfraLogger.bindings();
-        // expect(loggerBindings).toEqual({service: 'audit', realm: 'infra'})
+        const loggerBindings = InfraLogger.bindings();
+        expect(loggerBindings).toEqual({ service: 'audit', realm: 'infra' });
     });
     it('DomainLogger has service set to audit and realm set to domain on bindings', () => {
-        // const loggerBindings = DomainLogger.bindings();
-        // expect(loggerBindings).toEqual({service: 'audit', realm: 'domain'})
+        const loggerBindings = DomainLogger.bindings();
+        expect(loggerBindings).toEqual({ service: 'audit', realm: 'domain' });
     });
 });
