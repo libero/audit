e1140d32f84a73ec411836a2956bc454fba39aff
Cory <c.donkin@elifesciences.org>
2020-11-27 15:37:42 +0000

    remove logging

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -3,6 +3,5 @@ import { Request, Response } from 'express';
 export type Endpoint = () => (req: Request, res: Response) => void;
 
 export const HealthCheck: Endpoint = () => (_req: Request, res: Response): void => {
-    console.log('Im up and running!!!!!!');
     res.status(200).json({ ok: true });
 };
